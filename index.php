f52d26e82c96d4b8335181d663b67bdedacbe91b
Gaston Marcilio <gmarcilio@sofrecom.com.ar>
2023-09-14 11:13:30 -0300

    3

--- index.php
+++ index.php
@@ -36,18 +36,15 @@ $todosLosWarehouse = $pedidos->traerWarehouse();
 <meta charset="UTF-8"></meta>
 <link rel="shortcut icon" href="assets/icono.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css" >
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<?php 
+	require_once $_SERVER['DOCUMENT_ROOT']. '/ecommerce/assets/css/css.php';
+?>
+
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet"/>
-<!-- Including Font Awesome CSS from CDN to show icons -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-<link rel="stylesheet" href="vista/style.css">
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+
 </head>
 
 <body>
@@ -298,12 +295,12 @@ $id++;
 
   
 </div>
-<?php 
-	require_once $_SERVER['DOCUMENT_ROOT']. '/ecommerce/assets/css/css.php';
-?>
 <!-- <script type="text/javascript" src="Controlador/main.js"></script> -->
 
 
+<?php 
+	require_once $_SERVER['DOCUMENT_ROOT']. '/ecommerce/assets/js/js.php';
+?>
 <script src="assets/bootstrap/popper.min.js" ></script>
 <script src="assets/bootstrap/bootstrap.min.js" ></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
